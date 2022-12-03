382e920377affe41765b87249baa38443cdfe2de
Yousef Na <yousefnasr0541@gmail.com>
2022-12-03 14:48:42 +0200

    improve firebase with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-actions";
+let isIntial = true;
 
 function App() {
+	const dispatch = useDispatch();
 	const showCart = useSelector((state) => state.ui.cartIsVisible);
+	const notification = useSelector((state) => state.ui.notification);
 	const cart = useSelector((state) => state.cart);
 
 	useEffect(() => {
-		fetch("https://react-http-60bbc-default-rtdb.firebaseio.com/cart.json", {
-			method: "PUT",
-			body: JSON.stringify(cart),
-		});
-	}, [cart]);
+		if (isIntial) {
+			isIntial = false;
+			return;
+		}
+		dispatch(sendCartData(cart));
+	}, [cart, dispatch]);
 
 	return (
-		<Layout>
-			{showCart && <Cart />}
-			<Products />
-		</Layout>
+		<>
+			{notification && (
+				<Notification
+					status={notification.status}
+					title={notification.title}
+					message={notification.message}
+				/>
+			)}
+			<Layout>
+				{showCart && <Cart />}
+				<Products />
+			</Layout>
+		</>
 	);
 }
 
